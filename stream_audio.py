66ac419698da70edbe299a67a1872a2f1b474be9
bokehsensei <bokeh.sensei@gmail.com>
2013-07-04 19:59:14 -0700

    better animation

--- stream_audio.py
+++ stream_audio.py
@@ -1,35 +1,36 @@
 import matplotlib
 matplotlib.use('TkAgg') # do this before importing pylab
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import struct
 
-time_window = 90000 # samples
-slide = 10000
-data = []
+slide = 4*1024 # samples
+#slide_ms = 1000*slide / 44100
+slide_ms = 10
+print('slide window: {0} milliseconds'.format(slide_ms))
+time_window = 20*slide
+samples = [float(0) for x in range(time_window)]
 try:
 	microphone = open('/tmp/microphone', 'rb')
 except IOError as e:
 	print(e)
 	sys.exit(1)
 
-for x in range(time_window):
-	data.append(struct.unpack('f',microphone.read(4))[0])
+unpacker = struct.Struct('{0}f'.format(slide))
+
+def read_more_sound_samples():
+	while True:
+		yield unpacker.unpack_from(microphone.read(unpacker.size))
+
+def update_plot(new_sound_samples, samples):
+	samples.extend(new_sound_samples)
+	samples = samples[-time_window:]
+	line.set_data(range(len(samples)), samples)
+	return line,
+
 fig = plt.figure()
 ax = fig.add_subplot(111)
-
-line, = ax.plot(data)
-
-def animate(*args):
-	n = len(data)
-	while True:
-		for x in range(slide):
-			data.append(struct.unpack('f',microphone.read(4))[0])
-		n += 1
-		line.set_data(range(n-slide, n), data[-slide:])
-		ax.autoscale_view()
-		ax.set_xlim(n-(slide+1), n-1)
-		fig.canvas.draw()
-
-fig.canvas.manager.window.after(100, animate)
+line, = ax.plot(samples)
+ani = animation.FuncAnimation(fig, update_plot, read_more_sound_samples, interval=slide_ms, fargs = (samples,))
 plt.show()
 microphone.close()
